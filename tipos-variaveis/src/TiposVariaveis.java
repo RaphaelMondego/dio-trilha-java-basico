bc0fd0e9386dd4e23bbc50805108c29a35b05231
Raphael Mondego <raphamondego@gmail.com>
2025-03-16 12:12:22 -0300

    Constantes - final

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        final double VALOR_DE_PI = 3.14; //final refere-se a uma constante e o valor dessa constante tem que ser em caixa alta.
+        
+        VALOR_DE_PI = 10.75;   // irá gerar um erro na linha
     }
 }
